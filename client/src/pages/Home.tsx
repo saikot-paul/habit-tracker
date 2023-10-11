68b3fb4c3af512d5574c817ad6e87019cfbd6e27
saikot-paul <saikotpaul123@gmail.com>
2023-10-11 00:27:00 -0400

    added template for navbar, not done yet

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import BasicDateCalender from '../components/calender/BasicDateCalender'
+import NavigationBar from "../components/navBar/NavigationBar";
 
 export default function Home() {
-    return (
-        <>
-            <BasicDateCalender/>
-        </>
-    )
+  return (
+    <>
+      <NavigationBar />
+      <BasicDateCalender />
+    </>
+  );
 }
