d689df14745ef5042298c1422a139bb67c450e5a
Andrew Gao <aggaodrew@gmail.com>
2023-11-03 01:48:07 -0400

    redirect user to calender page
    user is redirected to the calendar page (home) after a successful login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
